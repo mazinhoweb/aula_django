74aa0e32d46d5b5655109f1800691453caf86188
Edmar Inocencio - Mazinho Web <mazinhoweb@outlook.com>
2016-10-26 04:40:01 -0200

    Admin adicionado

--- settings.py
+++ settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'aula_django.core',
 ]
 
 MIDDLEWARE = [
@@ -103,15 +104,17 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
 USE_L10N = True
 
 USE_TZ = True
+
+LOGIN_URL = '/admin/login'
 
 
 # Static files (CSS, JavaScript, Images)
--- urls.py
+++ urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from aula_django.core.views import home
 
 urlpatterns = [
+    url(r'^$', home),
     url(r'^admin/', admin.site.urls),
 ]
+
